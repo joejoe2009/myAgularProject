4a7f05befb57188d7694d53e2aba7f6ad7fcb31f
joejoe2009 <rexgmann@gmail.com>
2022-09-15 10:21:21 -0700

    adding to shopping list

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,6 +11,8 @@ import { RecipeItemComponent } from './recipes/recipe-list/recipe-item/recipe-it
 import { ShoppingListComponent } from './shopping-list/shopping-list.component';
 import { ShoppingEditComponent } from './shopping-list/shopping-edit/shopping-edit.component';
 import { DropdownDirective } from './shared/dropdown.directive';
+import { ShoppingListService } from './shopping-list/shopping-list.service';
+
 
 @NgModule({
   declarations: [
@@ -22,13 +24,14 @@ import { DropdownDirective } from './shared/dropdown.directive';
     RecipeItemComponent,
     ShoppingListComponent,
     ShoppingEditComponent,
-    DropdownDirective
+    DropdownDirective,
+
   ],
   imports: [
     BrowserModule,
     FormsModule
   ],
-  providers: [],
+  providers: [ShoppingListService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,6 +1,7 @@
 import { Component, EventEmitter, OnInit, Output } from '@angular/core';
 
 import {Recipe} from '../recipe.model';
+import { RecipeService } from '../recipe.service';
 
 @Component({
   selector: 'app-recipe-list',
@@ -8,21 +9,22 @@ import {Recipe} from '../recipe.model';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
-  @Output() recipeWasSelected = new EventEmitter<Recipe>();
+  // @Output() recipeWasSelected = new EventEmitter<Recipe>();
 
   recipes: Recipe[] = [
-    new Recipe('A Test Recipe', 'This is simply a test', 'https://www.seriouseats.com/thmb/u8LF_VE-D3zStw-r24fhWleYieM=/880x0/filters:no_upscale():max_bytes(150000):strip_icc():format(webp)/20220525-LAVASH-BEAN-TRIANGLES-ANDREW-JANJIGIAN-Hero-aa7b65a0f62246a9924066a57c964548.JPG'),
-    new Recipe('Another Test Recipe', 'This is simply a test', 'https://www.seriouseats.com/thmb/u8LF_VE-D3zStw-r24fhWleYieM=/880x0/filters:no_upscale():max_bytes(150000):strip_icc():format(webp)/20220525-LAVASH-BEAN-TRIANGLES-ANDREW-JANJIGIAN-Hero-aa7b65a0f62246a9924066a57c964548.JPG')
+    // new Recipe('A Test Recipe', 'This is simply a test', 'https://www.seriouseats.com/thmb/u8LF_VE-D3zStw-r24fhWleYieM=/880x0/filters:no_upscale():max_bytes(150000):strip_icc():format(webp)/20220525-LAVASH-BEAN-TRIANGLES-ANDREW-JANJIGIAN-Hero-aa7b65a0f62246a9924066a57c964548.JPG'),
+    // new Recipe('Another Test Recipe', 'This is simply a test', 'https://www.seriouseats.com/thmb/u8LF_VE-D3zStw-r24fhWleYieM=/880x0/filters:no_upscale():max_bytes(150000):strip_icc():format(webp)/20220525-LAVASH-BEAN-TRIANGLES-ANDREW-JANJIGIAN-Hero-aa7b65a0f62246a9924066a57c964548.JPG')
   ];
   
 
-  constructor() { }
+  constructor(private recipeService: RecipeService) { }
 
   ngOnInit(): void {
+    this.recipes = this.recipeService.getRecipe();
   }
 
-  onRecipeSelected(recipe: Recipe) {
-    this.recipeWasSelected.emit(recipe);
-  }
+  // onRecipeSelected(recipe: Recipe) {
+  //   this.recipeWasSelected.emit(recipe);
+  // }
 
 }
